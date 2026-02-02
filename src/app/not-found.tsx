ffbf41f175b419cb9e21f897c67c977a331edd8d
GKFfidele <goussikindefidele20@gmail.com>
2026-02-02 15:12:58 +0100

    revnir derrire

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,9 +7,9 @@ export default function NotFound() {
         404
       </Text>
       <Heading marginBottom="l" variant="display-default-xs">
-        Page Not Found
+        Page Non trouvé
       </Heading>
-      <Text onBackground="neutral-weak">The page you are looking for does not exist.</Text>
+      <Text onBackground="neutral-weak">La page que vous cherche n'existe pas.</Text>
     </Column>
   );
 }
